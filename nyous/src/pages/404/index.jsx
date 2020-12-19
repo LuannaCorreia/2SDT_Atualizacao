a0814cba9f6b9963b70c5391f56612955300f337
BrenoPierin <brenomax02@gmail.com>
2020-12-18 22:56:18 -0200

    Aulas 9, 10, 12 e 13 atualizadas e concluídas com sucesso

---
+++ nyous/src/pages/404/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Menu from "../../components/menu";
+import Rodape from "../../components/rodape";
+
+const NaoEncontrada = () => {
+    return (
+        <div>
+            <Menu/>
+            <h1>Error 404 - Página não Encontrada</h1>
+            <Rodape/>
+        </div>
+    )
+}
+
+export default NaoEncontrada;
